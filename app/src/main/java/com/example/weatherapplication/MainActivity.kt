b4e08af2f2e1452b31ed396aabfa444cfc6b852a
Semir Hodzic <semir@smartson.se>
2020-11-27 15:02:46 +0100

    Get temp from API and updateUI1
    Added the functionality to get weather data from openWeatherMap api. Get temp and update user interface accordingly.

--- app/src/main/java/com/example/weatherapplication/MainActivity.kt
+++ app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -5,28 +5,42 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.gson.Gson
 import okhttp3.*
-import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
+import kotlin.properties.Delegates
 
 
 class MainActivity : AppCompatActivity() {
+    private var temperatureTextView: TextView? = null
+
     private val client = OkHttpClient()
+    private val API_URL =
+        "https://api.openweathermap.org/data/2.5/weather?q=stockholm&units=metric&appid=8df6e9cbc37e2471dea928884f364bf3"
+
+    private var tempString: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val getCurrentWeatherButton : Button = findViewById(R.id.getWeatherButton)
-        val textView : TextView = findViewById<TextView>(R.id.temperature)
 
-        run("https://api.openweathermap.org/data/2.5/weather?q=stockholm&units=metric&appid=8df6e9cbc37e2471dea928884f364bf3")
+        val getCurrentWeatherButton: Button = findViewById(R.id.getWeatherButton)
+        getCurrentWeatherButton?.setOnClickListener()
+        {
+            getWeatherAsync()
+        }
+
+        val textView: TextView = findViewById<TextView>(R.id.temperature)
+        temperatureTextView = textView;
     }
 
-    fun run(url: String) {
+    private fun getWeatherAsync() {
+
         val request = Request.Builder()
-            .url(url)
+            .url(API_URL)
             .build()
 
         client.newCall(request).enqueue(object : Callback {
@@ -38,20 +52,40 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call, response: Response) {
 
-                if (!response.isSuccessful) {
-                    throw IOException("Unexpected code " + response);
-                }
-                try {
-                    val responseData = response.body()!!.string()
-                    val json = JSONObject(responseData)
-                    val name = json.getString("name")
+                val responseString = response.body()!!.string()
 
-                    Log.d("Name: ", "Name from json object: " + name.toString())
+                /**
+                 * Parse JSON response to Gson library
+                 */
 
-                } catch (e: JSONException) {
+                var jsonObject: JSONObject? = JSONObject(responseString)
+                val gson = Gson()
+                val weatherData: WeatherData = gson.fromJson(
+                    jsonObject.toString(),
+                    WeatherData::class.java
+                )
 
-                }
+                Thread(Runnable {
+                    runOnUiThread {
+                        updateUI(weatherData)
+                    }
+                }).start()
             }
         })
+    }
+
+    private fun updateUI(weatherData: WeatherData?) {
+        if (weatherData != null) {
+
+            var name: String by Delegates.observable("<>") {
+                    prop, old, new ->
+                Log.d("DEBUG","OBSERVER: $old -> $new")
+            }
+
+            tempString = weatherData.main.temp.toString()
+            name = tempString as String
+            temperatureTextView?.text = "Temp in Sthlm is: $name Celsius"
+
+        }
     }
 }
