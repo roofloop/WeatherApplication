f4695ee58397417ca6db3be0b9aeb04bb853694c
Semir Hodzic <semir@smartson.se>
2020-12-16 14:32:17 +0100

    Check if network is available and set the boolean isNetworkConnected from the object singleton accordingly

---
+++ app/src/main/java/com/example/weatherapplication/NetworkMonitor.kt
@@ -0,0 +1,40 @@
+package com.example.weatherapplication
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import androidx.annotation.RequiresPermission
+import com.example.weatherapplication.Model.Variables
+
+class NetworkMonitor
+@RequiresPermission(android.Manifest.permission.ACCESS_NETWORK_STATE)
+constructor(private val application: Application)
+{
+
+    fun startNetworkCallback() {
+        val cm: ConnectivityManager =
+                application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val builder: NetworkRequest.Builder = NetworkRequest.Builder()
+
+        cm.registerNetworkCallback(
+                builder.build(),
+                object : ConnectivityManager.NetworkCallback() {
+
+                    override fun onAvailable(network: Network) {
+                        Variables.isNetworkConnected = true
+                    }
+
+                    override fun onLost(network: Network) {
+                        Variables.isNetworkConnected = false
+                    }
+                })
+    }
+
+    fun stopNetworkCallback() {
+        val cm: ConnectivityManager =
+                application.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        cm.unregisterNetworkCallback(ConnectivityManager.NetworkCallback())
+    }
+}
