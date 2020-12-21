4712cce3d4c87dc8b54a2d7c8a4fca841016d622
Semir Hodzic <semir@smartson.se>
2020-12-21 13:40:36 +0100

    handleNetworkChanges is implemented in activity

--- app/src/main/java/com/example/weatherapplication/Activity/SelectedActivity.kt
+++ app/src/main/java/com/example/weatherapplication/Activity/SelectedActivity.kt
@@ -1,17 +1,16 @@
 package com.example.weatherapplication.Activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import com.example.weatherapplication.Interface.PostFirestoreModel
+import com.example.weatherapplication.NetworkUtils
 import com.example.weatherapplication.R
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_selected.*
-
 
 
 class SelectedActivity : AppCompatActivity() {
@@ -21,6 +20,9 @@ class SelectedActivity : AppCompatActivity() {
     private lateinit var firestoreHelper:PostFirestoreModel
     var TAG:String? = "SelectedActivity"
 
+    private lateinit var deleteButton: Button
+    private lateinit var updateButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_selected)
@@ -29,30 +31,43 @@ class SelectedActivity : AppCompatActivity() {
         diaryInput = intent.getStringExtra("diaryInput")
         temp = intent.getStringExtra("temp")
 
+        deleteButton = findViewById(R.id.deleteButton)
+        updateButton = findViewById(R.id.saveButton)
+
         textViewEditText.setText(diaryInput.toString())
         textViewTemp.text = temp
 
-        val deleteButton = findViewById<Button>(R.id.deleteButton)
-        deleteButton.setOnClickListener { deleteFromFirestore() }
-
-        val updateButton = findViewById<Button>(R.id.saveButton)
-        updateButton.setOnClickListener { updateFirestore() }
+        handleNetworkChanges()
 
     }
+
+    private fun handleNetworkChanges()
+    {
+
+        NetworkUtils.getNetworkLiveData(applicationContext).observe(this, { isConncted ->
+            if (!isConncted)
+            {
+                deleteButton.setOnClickListener { Toast.makeText(this, "Network is missing", Toast.LENGTH_SHORT).show() }
+                updateButton.setOnClickListener { Toast.makeText(this, "Network is missing", Toast.LENGTH_SHORT).show() }
+
+            }else{
+                deleteButton.setOnClickListener { deleteFromFirestore() }
+                updateButton.setOnClickListener { updateFirestore() }
+
+            }
+        })
+    }
+
 
     private fun deleteFromFirestore(){
         val db = Firebase.firestore
 
         firestoreHelper = PostFirestoreModel()
         firestoreHelper.deleteFromFirestore(id!!,db)
-
         Toast.makeText(this,"Success", Toast.LENGTH_SHORT).show()
-
-        startActivity(Intent(this, MainActivity::class.java))
         finish()
 
     }
-
 
     private fun updateFirestore(){
         val db = Firebase.firestore
@@ -62,10 +77,8 @@ class SelectedActivity : AppCompatActivity() {
         firestoreHelper.updateToFirestore(id!!,textViewEditText.text.toString(),db  )
 
         Log.d(TAG, "textViewEditText: " + textViewEditText.text.toString())
-
         Toast.makeText(this,"Success", Toast.LENGTH_SHORT).show()
 
-        startActivity(Intent(this, MainActivity::class.java))
         finish()
 
     }
@@ -74,5 +87,4 @@ class SelectedActivity : AppCompatActivity() {
         super.onBackPressed()
         finish()
     }
-
 }
