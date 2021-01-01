035b6a3c7f6ee95f59fc33f0bf734bcf5a372f53
roofloop <55179429+roofloop@users.noreply.github.com>
2021-01-01 15:54:39 +0100

    Renaming of list

--- app/src/main/java/com/example/weatherapplication/Interface/PostFirestoreModel.kt
+++ app/src/main/java/com/example/weatherapplication/Interface/PostFirestoreModel.kt
@@ -5,12 +5,9 @@ import android.content.Context
 import android.util.Log
 import com.example.weatherapplication.Model.CacheModel
 import com.example.weatherapplication.Model.PostFirestore
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.firestoreSettings
 import com.google.firebase.ktx.Firebase
-import java.lang.Exception
-import javax.security.auth.callback.Callback
 
 
 class PostFirestoreModel : PostFirestoreInterface {
@@ -23,27 +20,34 @@ class PostFirestoreModel : PostFirestoreInterface {
     }
 
     override fun getFromFirestore(context: Context, callback: (MutableList<PostFirestore>) -> Unit) {
-        db.firestoreSettings = settings
-        val notesList = mutableListOf<PostFirestore>()
+        val diaryInputsList = mutableListOf<PostFirestore>()
         try {
-            db.collection("InputsDiary")
+            db.collection("DiaryInputs")
                     .addSnapshotListener { snapshot, e ->
-                        notesList.clear()
+                        diaryInputsList.clear()
 
                         if (snapshot != null && !snapshot.isEmpty) {
                             for (doc in snapshot.documents) {
                                 val note = doc.toObject(PostFirestore::class.java)
-                                notesList.add(note!!)
-                                cacheHelper.createCachedFile(context, notesList)
+                                diaryInputsList.add(note!!)
                             }
 
-                            callback(notesList)
 
+                           val cacheList = cacheHelper.readCachedFile(context)
+
+                            val finalList = ArrayList<Any>()
+                            finalList.addAll(cacheList)
+                            finalList.addAll(diaryInputsList)
+
+                            cacheHelper.createCachedFile(context, finalList as MutableList<PostFirestore>)
+
+                            
+                            callback(diaryInputsList)
 
                         } else {
                             //Refreshing the RV and cache if firestore is empty.
                             cacheHelper.deleteCachedFile(context)
-                            callback(notesList)
+                            callback(diaryInputsList)
                         }
                     }
         } catch (e: Exception){
@@ -55,7 +59,7 @@ class PostFirestoreModel : PostFirestoreInterface {
     override fun addToFirestore(postFirestore: PostFirestore) {
 
         try {
-            val newDiaryInputRef = db.collection("InputsDiary").document()
+            val newDiaryInputRef = db.collection("DiaryInputs").document()
             postFirestore.id = newDiaryInputRef.id
 
             // Add a new document with a generated ID
@@ -63,7 +67,7 @@ class PostFirestoreModel : PostFirestoreInterface {
                 .set(postFirestore)
                 .addOnSuccessListener {
 
-                    Log.d(TAG, "DocumentSnapshot added with ID:" + newDiaryInputRef.id )
+                    Log.d(TAG, "DocumentSnapshot added with ID:" + newDiaryInputRef.id)
 
                 }
                 .addOnFailureListener { e ->
@@ -76,7 +80,7 @@ class PostFirestoreModel : PostFirestoreInterface {
     }
     override fun deleteFromFirestore(id: String) {
 
-        db.collection("InputsDiary").document(id)
+        db.collection("DiaryInputs").document(id)
             .delete()
             .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully deleted!") }
             .addOnFailureListener { e ->
@@ -85,7 +89,7 @@ class PostFirestoreModel : PostFirestoreInterface {
     }
 
     override fun updateToFirestore(id: String, diaryInput: String) {
-        val updateDiaryInputRef = db.collection("InputsDiary").document(id)
+        val updateDiaryInputRef = db.collection("DiaryInputs").document(id)
 
         updateDiaryInputRef
             .update("diaryInput", diaryInput)
