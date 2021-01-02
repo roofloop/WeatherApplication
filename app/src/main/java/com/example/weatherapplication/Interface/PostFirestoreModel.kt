29ea7e5bcdd634ba7fc07607990e8cc87a7b454b
Semir Hodzic <semir@smartson.se>
2021-01-02 14:02:18 +0100

    No combining of lists necessary in this place

--- app/src/main/java/com/example/weatherapplication/Interface/PostFirestoreModel.kt
+++ app/src/main/java/com/example/weatherapplication/Interface/PostFirestoreModel.kt
@@ -8,6 +8,7 @@ import com.example.weatherapplication.Model.PostFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.firestoreSettings
 import com.google.firebase.ktx.Firebase
+import java.io.FileNotFoundException
 
 
 class PostFirestoreModel : PostFirestoreInterface {
@@ -15,12 +16,11 @@ class PostFirestoreModel : PostFirestoreInterface {
     private val TAG: String = "PostFirestoreModel"
     private val db = Firebase.firestore
     private val cacheHelper: CacheModel = CacheModel()
-    private val settings = firestoreSettings {
-        isPersistenceEnabled = false
-    }
 
     override fun getFromFirestore(context: Context, callback: (MutableList<PostFirestore>) -> Unit) {
+
         val diaryInputsList = mutableListOf<PostFirestore>()
+
         try {
             db.collection("DiaryInputs")
                     .addSnapshotListener { snapshot, e ->
@@ -28,24 +28,19 @@ class PostFirestoreModel : PostFirestoreInterface {
 
                         if (snapshot != null && !snapshot.isEmpty) {
                             for (doc in snapshot.documents) {
-                                val note = doc.toObject(PostFirestore::class.java)
-                                diaryInputsList.add(note!!)
+                                val diaryInputs = doc.toObject(PostFirestore::class.java)
+
+                                // Adding data from firestore to out mutableList
+                                diaryInputsList.add(diaryInputs!!)
                             }
+                            // Creating our cache file (or overwriting existing), with fresh data from firestore.
+                            cacheHelper.createCachedFile(context, diaryInputsList)
 
-
-                           val cacheList = cacheHelper.readCachedFile(context)
-
-                            val finalList = ArrayList<Any>()
-                            finalList.addAll(cacheList)
-                            finalList.addAll(diaryInputsList)
-
-                            cacheHelper.createCachedFile(context, finalList as MutableList<PostFirestore>)
-
-                            
+                            // Returning the up to date mutableList
                             callback(diaryInputsList)
 
                         } else {
-                            //Refreshing the RV and cache if firestore is empty.
+                            //Refreshing the RV and deleting the cache if firestore is empty.
                             cacheHelper.deleteCachedFile(context)
                             callback(diaryInputsList)
                         }
@@ -53,7 +48,6 @@ class PostFirestoreModel : PostFirestoreInterface {
         } catch (e: Exception){
             Log.d(TAG, "Failure", e)
         }
-
     }
 
     override fun addToFirestore(postFirestore: PostFirestore) {
@@ -64,37 +58,36 @@ class PostFirestoreModel : PostFirestoreInterface {
 
             // Add a new document with a generated ID
             newDiaryInputRef
-                .set(postFirestore)
-                .addOnSuccessListener {
+                    .set(postFirestore)
+                    .addOnSuccessListener {
 
-                    Log.d(TAG, "DocumentSnapshot added with ID:" + newDiaryInputRef.id)
+                        Log.d(TAG, "DocumentSnapshot added with ID:" + newDiaryInputRef.id)
 
-                }
-                .addOnFailureListener { e ->
-                    Log.w(TAG, "Error adding document", e)
-                }
+                    }
+                    .addOnFailureListener { e ->
+                        Log.w(TAG, "Error adding document", e)
+                    }
 
         } catch (e: Exception){
             Log.w(TAG, "Error adding to firestore", e)
         }
     }
     override fun deleteFromFirestore(id: String) {
-
         db.collection("DiaryInputs").document(id)
-            .delete()
-            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully deleted!") }
-            .addOnFailureListener { e ->
-                Log.w(TAG, "Error deleting document", e)
-            }
+                .delete()
+                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully deleted!") }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error deleting document", e)
+                }
     }
 
     override fun updateToFirestore(id: String, diaryInput: String) {
         val updateDiaryInputRef = db.collection("DiaryInputs").document(id)
 
         updateDiaryInputRef
-            .update("diaryInput", diaryInput)
-            .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully updated!") }
-            .addOnFailureListener { e -> Log.w(TAG, "Error updating document", e) }
+                .update("diaryInput", diaryInput)
+                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully updated!") }
+                .addOnFailureListener { e -> Log.w(TAG, "Error updating document", e) }
 
     }
 }
